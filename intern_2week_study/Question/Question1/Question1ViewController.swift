7a2754e40d9147ced76e98aabfc2e7d9c0e11d07
ryo0311 <18eh025@ms.dendai.ac.jp>
2020-08-12 18:35:14 +0900

    課題１　編集

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -5,8 +5,29 @@ final class Question1ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var addTextButton: UIButton!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var comment: UITextView!
     
+
+    @IBAction func button(_ sender: Any) {
+     if(textField.text==""){
+        label.text="文字を入力してください"
+        textView.textColor = UIColor.red
+        
+     }else{
+             comment.text += textField.text! + "\n"
+             label.text = ""
+      }
+        print(comment.text)
+  }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func clearbutton(_ sender: Any) {
+        comment.text=""
+        
+    }
+    
 }
+
